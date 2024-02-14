67909feb6aaaab4f86353dc93526f6d2247795ca
eliran3 <elirancoding@gmail.com>
2024-02-14 11:25:44 +0200

    Add: magit package installation

--- emacs
+++ emacs
@@ -7,13 +7,19 @@
  '(custom-safe-themes
    '("db2aff20caf2b09e8f3c1f4a36c136c8dfeb8d02e1d0980e7daff2a0196d7fab" default))
  '(package-selected-packages
-   '(json-mode prisma-mode js2-mode lsp-prisma el-get zig-mode reformatter yaml-mode typescript-mode dockerfile-mode)))
+   '(magit json-mode prisma-mode js2-mode lsp-prisma el-get zig-mode reformatter yaml-mode typescript-mode dockerfile-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(line-number-current-line ((t (:inherit line-number :foreground "yellow" :weight normal)))))
+
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
 
 (setq display-line-numbers-type 'relative) 
 (global-display-line-numbers-mode)
--- emacs.d/elpa/prisma-mode-0.1/prisma-mode-autoloads.el
+++ emacs.d/elpa/prisma-mode-0.1/prisma-mode-autoloads.el
@@ -6,8 +6,22 @@
                          (or (file-name-directory #$) (car load-path))))
 
 
-;;;### (autoloads nil "prisma-mode" "prisma-mode.el" (0 0 0 0))
+;;;### (autoloads nil "../../../Documents/Emacs-Configuration/emacs.d/elpa/prisma-mode-0.1/prisma-mode"
+;;;;;;  "prisma-mode.el" "fd35133b4334be2b0a36d9c6c03e1add")
 ;;; Generated autoloads from prisma-mode.el
+
+(autoload 'prisma-mode "../../../Documents/Emacs-Configuration/emacs.d/elpa/prisma-mode-0.1/prisma-mode" "\
+Major mode for editing Prisma data models.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.prisma$" . prisma-mode))
+
+;;;***
+
+;;;### (autoloads nil "prisma-mode" "../../../../../.emacs.d/elpa/prisma-mode-0.1/prisma-mode.el"
+;;;;;;  "fd35133b4334be2b0a36d9c6c03e1add")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/prisma-mode-0.1/prisma-mode.el
 
 (autoload 'prisma-mode "prisma-mode" "\
 Major mode for editing Prisma data models.
@@ -15,6 +29,13 @@ Major mode for editing Prisma data models.
 \(fn)" t nil)
 
 (add-to-list 'auto-mode-alist '("\\.prisma$" . prisma-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/prisma-mode-0.1/prisma-mode-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/prisma-mode-0.1/prisma-mode-pkg.el"
+;;;;;;  "../../../../../.emacs.d/elpa/prisma-mode-0.1/prisma-mode.el"
+;;;;;;  "prisma-mode.el") (0 0 0 0))
 
 ;;;***
 
